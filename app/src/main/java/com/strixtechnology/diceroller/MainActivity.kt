08e85e8756bb2f1f1d1d53ed0234d836868eb1ab
SefisoMonama <sfisomonama998@gmail.com>
2020-12-08 13:32:47 +0200

    create dice app

---
+++ app/src/main/java/com/strixtechnology/diceroller/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.strixtechnology.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * This method is called when the Activity is created.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Find the Button in the layout
+        val rollButton: Button = findViewById(R.id.button)
+
+        // Set a click listener on the button to roll the dice when the user taps the button
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        var diceImage: ImageView = findViewById(R.id.imageView)
+        when (diceRoll) {
+            //1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+
+    }
+}
+
+/**
+ * Dice with a fixed number of sides.
+ */
+class Dice(private val numSides: Int) {
+
+    /**
+     * Do a random dice roll and return the result.
+     */
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
