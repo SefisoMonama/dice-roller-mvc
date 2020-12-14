06df346715d720eb4bbdc14e7299ef8fe7409b61
SefisoMonama <sfisomonama998@gmail.com>
2020-12-14 13:49:56 +0200

    declare local var to functions

--- app/src/main/java/com/strixtechnology/diceroller/Dice.kt
+++ app/src/main/java/com/strixtechnology/diceroller/Dice.kt
@@ -25,7 +25,7 @@ class Dice(private val numSides: Int) {
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
             6 -> R.drawable.dice_6
             else ->  R.drawable.dice_1
         }
--- app/src/test/java/com/strixtechnology/diceroller/DiceTest.kt
+++ app/src/test/java/com/strixtechnology/diceroller/DiceTest.kt
@@ -24,48 +24,54 @@ class DiceTest : TestCase() {
     @Test
     fun testCorrectDrawableLoadsIfDiceRollsSix() {
         val expectedValue = R.drawable.dice_6
+        val side_6: Int=6
         //TODO: write code here which sets the dice value to 6
-        dice.currentDiceValue =  DICE_SIDE_COUNT
+        dice.currentDiceValue = side_6
         val actualValue = dice.getDiceImageResource()
         Assert.assertEquals(expectedValue, actualValue)
     }
     @Test
     fun testCorrectDrawableLoadsIfDiceRollsFive() {
-        val expectedValue = R.drawable.dice_6
+        val expectedValue = R.drawable.dice_5
+        val side_5: Int=5
         //TODO: write code here which sets the dice value to 6
-        dice.currentDiceValue =  DICE_SIDE_COUNT
+        dice.currentDiceValue = side_5
         val actualValue = dice.getDiceImageResource()
         Assert.assertEquals(expectedValue, actualValue)
     }
     @Test
     fun testCorrectDrawableLoadsIfDiceRollsFour() {
-        val expectedValue = R.drawable.dice_6
+        val expectedValue = R.drawable.dice_4
+        val side_4: Int=4
         //TODO: write code here which sets the dice value to 6
-        dice.currentDiceValue =  DICE_SIDE_COUNT
+        dice.currentDiceValue =  side_4
         val actualValue = dice.getDiceImageResource()
         Assert.assertEquals(expectedValue, actualValue)
     }
     @Test
     fun testCorrectDrawableLoadsIfDiceRollsThree() {
-        val expectedValue = R.drawable.dice_6
+        val expectedValue = R.drawable.dice_3
+        val side_3: Int=3
         //TODO: write code here which sets the dice value to 6
-        dice.currentDiceValue =  DICE_SIDE_COUNT
+        dice.currentDiceValue =  side_3
         val actualValue = dice.getDiceImageResource()
         Assert.assertEquals(expectedValue, actualValue)
     }
     @Test
     fun testCorrectDrawableLoadsIfDiceRollsTwo() {
-        val expectedValue = R.drawable.dice_6
+        val expectedValue = R.drawable.dice_2
+        val side_2: Int=2
         //TODO: write code here which sets the dice value to 6
-        dice.currentDiceValue =  DICE_SIDE_COUNT
+        dice.currentDiceValue =  side_2
         val actualValue = dice.getDiceImageResource()
         Assert.assertEquals(expectedValue, actualValue)
     }
     @Test
     fun testCorrectDrawableLoadsIfDiceRollsOne() {
-        val expectedValue = R.drawable.dice_6
+        val expectedValue = R.drawable.dice_1
+        val side_1: Int=1
         //TODO: write code here which sets the dice value to 6
-        dice.currentDiceValue =  DICE_SIDE_COUNT
+        dice.currentDiceValue =  side_1
         val actualValue = dice.getDiceImageResource()
         Assert.assertEquals(expectedValue, actualValue)
     }
