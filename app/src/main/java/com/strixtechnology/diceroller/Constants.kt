57f7175f39901e231cd0f2da0304e9be87974d2a
dieterbarnard <dieter@strixtechnology.com>
2021-01-18 13:40:29 +0200

    Added SharedPreferences to app with basic usage

---
+++ app/src/main/java/com/strixtechnology/diceroller/Constants.kt
@@ -0,0 +1,7 @@
+package com.strixtechnology.diceroller
+
+const val KEY_DICE_COUNT = "number_of_dice"
+const val KEY_DICE_SIDES = "number_of_sides"
+
+const val DEFAULT_DICE_COUNT = 1
+const val DEFAULT_DICE_SIDES = 6
--- app/src/main/java/com/strixtechnology/diceroller/SettingsActivity.kt
+++ app/src/main/java/com/strixtechnology/diceroller/SettingsActivity.kt
@@ -1,19 +1,58 @@
 package com.strixtechnology.diceroller
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import com.strixtechnology.diceroller.databinding.ActivityMainBinding
+
 import com.strixtechnology.diceroller.databinding.ActivitySettingsBinding
 
 private lateinit var binding: ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-        setContentView(R.layout.activity_settings)
+        setContentView(binding.root)
+        setInitialValues()
+        setChipGroupListeners()
     }
+
+    private fun getDiceCountFromStorage(): Int {
+        val prefs = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE)
+        return prefs.getInt(KEY_DICE_COUNT, DEFAULT_DICE_COUNT)
+    }
+
+    private fun storeDiceCount(count : Int) {
+        val prefs = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE)
+        with (prefs.edit()) {
+            putInt(KEY_DICE_COUNT, count)
+            apply()
+        }
+    }
+
+    private fun getSideCountFromStorage() : Int {
+        //TODO: IMPLEMENT THIS METHOD
+        return 6
+    }
+
+    private fun setInitialValues(){
+        val diceCount = getDiceCountFromStorage()
+        if (diceCount == 1){
+            binding.numberOfDice.check(R.id.roll1Dice)
+        }else {
+            binding.numberOfDice.check(R.id.roll2Dices)
+        }
+        //TODO: Set the initial values for the other ChipGroup
+    }
+
+    private fun setChipGroupListeners() {
+        binding.numberOfDice.setOnCheckedChangeListener { group, checkedId ->
+            when(checkedId){
+                R.id.roll1Dice -> storeDiceCount(1)
+                R.id.roll2Dices -> storeDiceCount(2)
+            }
+        }
+        //TODO: Set the listener for the other chipGroup
+
+    }
+
 }
