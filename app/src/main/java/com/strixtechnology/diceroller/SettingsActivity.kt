9b69eaad7aa63c8d5a6f89f848666b592e0c6963
SefisoMonama <sfisomonama998@gmail.com>
2021-01-18 17:03:13 +0200

    save checked chips using sharedPreferemces

--- app/src/main/java/com/strixtechnology/diceroller/SettingsActivity.kt
+++ app/src/main/java/com/strixtechnology/diceroller/SettingsActivity.kt
@@ -1,4 +1,5 @@
 package com.strixtechnology.diceroller
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -21,34 +22,57 @@ class SettingsActivity : AppCompatActivity() {
         return prefs.getInt(KEY_DICE_COUNT, DEFAULT_DICE_COUNT)
     }
 
-    private fun storeDiceCount(count : Int) {
+    private fun storeDiceCount(count: Int) {
         val prefs = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE)
-        with (prefs.edit()) {
+        with(prefs.edit()) {
             putInt(KEY_DICE_COUNT, count)
             apply()
         }
     }
 
-    private fun getSideCountFromStorage() : Int {
-        //TODO: IMPLEMENT THIS METHOD
-        return 6
+    //set dice sides to defaults values
+    private fun getSideCountFromStorage(): Int {
+        val pref = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE)
+        return pref.getInt(KEY_DICE_SIDES, DEFAULT_DICE_SIDES)
     }
 
-    private fun setInitialValues(){
+    //store number of dice side in sides variable
+    private fun storeSidesCount(sides: Int) {
+        val pref = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE)
+        with(pref.edit()) {
+            putInt(KEY_DICE_SIDES, sides)
+            apply()
+        }
+    }
+
+    private fun setInitialValues() {
         val diceCount = getDiceCountFromStorage()
-        if (diceCount == 1){
+        val sidesCount = getSideCountFromStorage()
+        if (diceCount == 1) {
             binding.numberOfDice.check(R.id.roll1Dice)
-        }else {
+        } else {
             binding.numberOfDice.check(R.id.roll2Dices)
         }
-        //TODO: Set the initial values for the other ChipGroup
+
+        if (sidesCount == 6) {
+            binding.chooseDiceSides.check(R.id.sixSides)
+        } else {
+            binding.chooseDiceSides.check(R.id.eightSides)
+        }
     }
 
     private fun setChipGroupListeners() {
         binding.numberOfDice.setOnCheckedChangeListener { group, checkedId ->
-            when(checkedId){
+            when (checkedId) {
                 R.id.roll1Dice -> storeDiceCount(1)
                 R.id.roll2Dices -> storeDiceCount(2)
+            }
+        }
+
+        binding.chooseDiceSides.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                R.id.sixSides -> storeSidesCount(6)
+                R.id.eightSides -> storeSidesCount(8)
             }
         }
         //TODO: Set the listener for the other chipGroup
